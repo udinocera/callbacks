7ed0a8eadfdcd3681629bcca0430051af81d8e89
udinocera <udinocera@gmail.com>
2023-09-09 23:25:41 +0200

    fine

--- exercise.js
+++ exercise.js
@@ -1,1 +1,10 @@
-
+function printAsyncName (callback, string){
+    setTimeout(() => callback(), 1000);
+    setTimeout(() => console.log(string), 2000)
+   }
+   
+   function callback(){
+       console.log("Hello");
+   }
+   
+   printAsyncName(callback, "Marco")
